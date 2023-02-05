1ab0b464955008ec0199fc2f31cbba11d20f897c
jrmenezes <jrbaptist25@gmail.com>
2023-02-05 00:30:21 -0300

    update_aula_01
    Instalando o Python
    Ambiente Virtual
    Framework Django
    Verificando o ambiente
    Instalando o django
    Iniciando um projeto
    Testando a instalação
    Criando uma App
    Testando os App's
    Investigando o Settings.py
    Rotas - urls.py
    views.py
    Inicializando o DB
    Criando uma Tabela (models.py)
    O Admin – Customizando
    Alterando uma tabela existente
    Customizando a exibição
    Customizando Campos
    Renderizando páginas HTML
    Permissões do Admin
    Customizando Ações no Admin

--- bdpratico/exemplo01/admin.py
+++ bdpratico/exemplo01/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import *
 
+@admin.action(description="Habilitar Registros Selecionados")
+def habilitar_pessoas(ModelAdmin, request, queryset):
+        for p in queryset:
+            p.ativo = True
+            p.save()
+
+@admin.action(description="Desabilitar Registros Selecionados")
+def desabilitar_pessoas(ModelAdmin, request, queryset):
+    queryset.update(ativo=False)
+
 class PessoaCustomizado(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'celular', 'funcao', 'ativo', )
+    list_display = ('nome', 'email', 'celular', 'funcao', 'nascimento', 'calcula_idade', 'ativo', )
+    actions = [habilitar_pessoas, desabilitar_pessoas]
+
+    @admin.display(description='Idade')
+    def calcula_idade(self, obj):
+        from datetime import date
+        hoje = date.today()
+        idade = hoje.year - obj.nascimento.year
+        return idade
 
 admin.site.register(pessoa, PessoaCustomizado)
--- bdpratico/exemplo01/models.py
+++ bdpratico/exemplo01/models.py
@@ -5,6 +5,7 @@ class pessoa(models.Model):
     email = models.CharField(max_length=50, null = False, blank = False, verbose_name = 'eMail')
     celular = models.CharField(max_length=20, null = True, blank = True, verbose_name = 'celular')
     funcao = models.CharField(max_length=30, null = True, blank = True, verbose_name = 'Funcao')
+    nascimento = models.DateField(null = False, blank = False, default ='1990-01-01', verbose_name = 'Nascimento')
     ativo = models.BooleanField(default = True, verbose_name = 'Ativo')
 
     def __str__(self):
